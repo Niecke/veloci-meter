b2833dd1d0b61dc0a27136f696a24152799f3af0
Daniel Niecke <niecke@bwl.uni-kiel.de>
2020-11-24 10:14:15 +0100

    clean up code

--- rdb/rdb.go
+++ rdb/rdb.go
@@ -72,7 +72,9 @@ func (r *Client) CountMail(pattern string) int64 {
 	sha1Hash := buildHash(pattern)
 	v, err := r.client.Eval("return #redis.pcall('keys', '"+sha1Hash+":*')", nil).Result()
 	if err != nil {
-		l.Errorf("[%v] Error while counting mails in redis.", err)
+		l.WithFields(l.Fields{
+			"error": err,
+		}).Errorf("[%v] Error while counting mails in redis.", err)
 		return int64(0)
 	} else {
 		l.WithFields(l.Fields{
@@ -89,16 +91,28 @@ func calculateGlobalKey(timestamp int, timeframe int) string {
 	return "global:" + fmt.Sprint(timeframe) + ":" + fmt.Sprint(keyPart)
 }
 
-// timefram in minutes
+// IncreaseGlobalCounter increments the global counter for the provided timeframe in minutes.
+// If the timeframe for example is 5 (minutes) then the function calculates the redis key for this timeframe based on the actual time and increments this counter.
+// If there is no redis key it will be 1 after this operation.
 func (r *Client) IncreaseGlobalCounter(timeframe int) {
 	timestamp := int(time.Now().Unix())
 	redisKey := calculateGlobalKey(timestamp, timeframe)
-	err := r.client.Incr(redisKey)
+	val, err := r.client.Incr(redisKey).Result()
 	if err != nil {
-		l.Debugf("[%v] Redis Command executed: [%v]", err, redisKey)
+		l.WithFields(l.Fields{
+			"error":     err,
+			"redis_key": redisKey,
+		}).Errorf("[%v] Redis Command executed: [%v]", err, redisKey)
+	} else {
+		l.WithFields(l.Fields{
+			"timeframe":    timeframe,
+			"redis_result": val,
+		}).Debugf("The global counter for timeframe %v minutes was increased.", timeframe)
 	}
 }
 
+// GetGlobalCounter returns the number of mails for the actual timeframe of n minutes.
+// If there has been no data in redis this function returns 0 since there have been no mails processed for this timestamp.
 func (r *Client) GetGlobalCounter(timeframe int) int {
 	timestamp := int(time.Now().Unix())
 	redisKey := calculateGlobalKey(timestamp, timeframe)
@@ -106,16 +120,32 @@ func (r *Client) GetGlobalCounter(timeframe int) int {
 
 	if err != nil {
 		if err == redis.Nil {
+			// If err == redis.Nil there has been no redis key for the actual timeframe thus no mails have been processed
+			l.WithFields(l.Fields{
+				"timeframe":    timeframe,
+				"redis_result": 0,
+			}).Debugf("There has been no data for timefram '%v' minutes thus 0 was returned.", timeframe)
 			return 0
 		}
-		l.Errorf("[%v] There was an error while getting global counter from redis. Redis key was %v", err, redisKey)
+		l.WithFields(l.Fields{
+			"error":     err,
+			"redis_key": redisKey,
+		}).Errorf("[%v] There was an error while getting global counter from redis. Redis key was %v", err, redisKey)
 	}
 
+	// Parse the result from redis into int
 	c, err := strconv.Atoi(val)
 	if err != nil {
-		l.Errorf("[%v] There parsing global counter value from redis. value was %v", err, val)
+		l.WithFields(l.Fields{
+			"error":        err,
+			"redis_result": val,
+		}).Errorf("[%v] There parsing global counter value from redis. value was %v", err, val)
 	}
 
+	l.WithFields(l.Fields{
+		"timeframe":    timeframe,
+		"redis_result": c,
+	}).Debugf("There have been %d mails for timefram '%v' minutes.", c, timeframe)
 	return c
 }
 
@@ -123,17 +153,28 @@ func (r *Client) GetKeys(pattern string) []string {
 	val, err := r.client.Keys(pattern).Result()
 
 	if err != nil {
-		l.Errorf("[%v] There was an error while getting keys from redis. Key pattern was %v", err, pattern)
+		l.WithFields(l.Fields{
+			"error":   err,
+			"pattern": pattern,
+		}).Errorf("[%v] There was an error while getting keys from redis. Key pattern was %v", err, pattern)
+		return []string{}
+	} else {
+		l.WithFields(l.Fields{
+			"pattern":      pattern,
+			"redis_result": val,
+		}).Debugf("There has been %d keys for pattern '%v'", len(val), pattern)
+		return val
 	}
-
-	return val
 }
 
 func (r *Client) DeleteKey(key string) int64 {
 	val, err := r.client.Del(key).Result()
 
 	if err != nil {
-		l.Errorf("[%v] There was an error while deleting %v from redis.", err, key)
+		l.WithFields(l.Fields{
+			"error":     err,
+			"redis_key": key,
+		}).Errorf("[%v] There was an error while deleting %v from redis.", err, key)
 	}
 
 	return val
